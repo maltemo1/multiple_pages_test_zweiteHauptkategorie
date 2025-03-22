67dd00c7a1bf49848561792b5481ec7e005b742b
maltemo1 <151633123+maltemo1@users.noreply.github.com>
2025-03-22 22:24:10 +0330

    Update multiple_pages_test_zweiteHauptkategorie.py

--- multiple_pages_test_zweiteHauptkategorie.py
+++ multiple_pages_test_zweiteHauptkategorie.py
@@ -33,7 +33,7 @@ def create_nav_structure():
                 "Deutschlands Top 10 Waren im Handel": "top10_goods_for_spec_country_all_time"
             },
             "Überblick nach bestimmtem Jahr": {
-                "Handelsbilanz & Ranking": "#",
+                "Handelsbilanz & Ranking": "LA_trade_spec_country_and_year",
                 "Monatlicher Handelsverlauf": "#",
                 "Top 10 Export- und Importwaren": "#",
                 "Top 4 Waren nach Differenz zum Vorjahr": "#",
@@ -139,7 +139,8 @@ graph_modules = [
     "gesamt_export_import_volumen",
     "monthly_trade", "top_10_trade_partners", "top_diff_countries", 
     "top_growth_countries", "top_diff_goods", "top_growth_goods", "top_10_trade_goods",
-    "LA_gesamt_export_import_volumen", "country_comparison", "export_import_growth_countries", "export_import_ranking_graph_of_country", "top10_goods_for_spec_country_all_time" # Hier neu hinzugefügt
+    "LA_gesamt_export_import_volumen", "country_comparison", "export_import_growth_countries", "export_import_ranking_graph_of_country", "top10_goods_for_spec_country_all_time",
+    "LA_trade_spec_country_and_year" # Hier neu hinzugefügt
 ]
 
 for module_name in graph_modules:
