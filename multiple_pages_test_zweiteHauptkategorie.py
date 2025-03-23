e4672c256377013528d822d802a04d670faddec8
maltemo1 <151633123+maltemo1@users.noreply.github.com>
2025-03-23 12:46:17 +0330

    Update multiple_pages_test_zweiteHauptkategorie.py

--- multiple_pages_test_zweiteHauptkategorie.py
+++ multiple_pages_test_zweiteHauptkategorie.py
@@ -35,8 +35,8 @@ def create_nav_structure():
             "Überblick nach bestimmtem Jahr": {
                 "Monatlicher Handelsverlauf, Handelsbilanz & Ranking": "#", #"LA_trade_spec_country_and_year",
                 "Top 10 Export- und Importwaren": "LA_top10_goods_for_spec_country_and_year",
-                "Top 4 Waren nach Differenz zum Vorjahr": "#",
-                "Top 4 Waren nach Wachstum zum Vorjahr": "#"
+                "Top 4 Waren nach Differenz zum Vorjahr": "top4_diff_goods_spec_country_and_year",
+                "Top 4 Waren nach Wachstum zum Vorjahr": "top4_growth_goods_spec_country_and_year"
             },
             "Überblick nach bestimmter Ware": {
                 "Gesamter Export- und Importverlauf der Ware mit Deutschland": "#"
@@ -139,7 +139,7 @@ graph_modules = [
     "monthly_trade", "top_10_trade_partners", "top_diff_countries", 
     "top_growth_countries", "top_diff_goods", "top_growth_goods", "top_10_trade_goods",
     "LA_gesamt_export_import_volumen", "country_comparison", "export_import_growth_countries", "export_import_ranking_graph_of_country", "top10_goods_for_spec_country_all_time",
-    "LA_top10_goods_for_spec_country_and_year" #, "LA_trade_spec_country_and_year"
+    "LA_top10_goods_for_spec_country_and_year", "top4_diff_goods_spec_country_and_year", "top4_growth_goods_spec_country_and_year" #, "LA_trade_spec_country_and_year"
 ]
 
 for module_name in graph_modules:
