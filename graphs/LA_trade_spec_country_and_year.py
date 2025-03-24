ce16d46d33746c734fb750fd0f81436ccd4818a9
maltemo1 <151633123+maltemo1@users.noreply.github.com>
2025-03-24 21:02:41 +0330

    Update LA_trade_spec_country_and_year.py

--- graphs/LA_trade_spec_country_and_year.py
+++ graphs/LA_trade_spec_country_and_year.py
@@ -127,7 +127,7 @@ def register_callbacks(app):
             status = df_selected['handelsbilanz_status'].values[0]
             handelsbilanz = df_selected['handelsbilanz'].values[0]
             
-            info_text = f"Handelsbilanzstatus im ausgewählten Jahr: {status} ({formatter(handelsbilanz)})"
+            info_text = f"Deutschlands Handelsbilanzstatus mit {selected_country} im ausgewählten Jahr: {status} ({handelsbilanz} €)"
         else:
             info_text = "Keine Daten zur Handelsbilanz verfügbar."
 
