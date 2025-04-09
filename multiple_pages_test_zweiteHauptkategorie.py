33b79024621f433b5730d4c8ba82928c8efc29f8
maltemo1 <151633123+maltemo1@users.noreply.github.com>
2025-04-09 11:48:49 +0200

    Update multiple_pages_test_zweiteHauptkategorie.py

--- multiple_pages_test_zweiteHauptkategorie.py
+++ multiple_pages_test_zweiteHauptkategorie.py
@@ -51,7 +51,7 @@ def create_nav_structure():
         },
         "Warenanalyse": {
             "Gesamtüberblick seit 2008 bis 2024": {
-                "Gesamter Export- und Importverlauf der Ware": "#",
+                "Gesamter Export- und Importverlauf der Ware": "overview_trade_spec_good_2008_until_2024",
                 "Deutschlands Top 5 Export- und Importländer der Ware": "#"
             },
             "Überblick mit mehreren Waren über bestimmten Zeitraum": {
@@ -140,7 +140,8 @@ graph_modules = [
     "top_growth_countries", "top_diff_goods", "top_growth_goods", "top_10_trade_goods",
     "LA_gesamt_export_import_volumen", "country_comparison", "export_import_growth_countries", "export_import_ranking_graph_of_country", "top10_goods_for_spec_country_all_time",
     "LA_top10_goods_for_spec_country_and_year", "top4_diff_goods_spec_country_and_year", "top4_growth_goods_spec_country_and_year", "LA_trade_spec_country_and_year",
-    "overview_trade_spec_good_with_spec_country_2008_until_2024", "trade_spec_country_and_several_goods_from_2008_2024", "trade_spec_good_and_several_countries_from_2008_2024"
+    "overview_trade_spec_good_with_spec_country_2008_until_2024", "trade_spec_country_and_several_goods_from_2008_2024", "trade_spec_good_and_several_countries_from_2008_2024",
+    "overview_trade_spec_good_2008_until_2024"
 ]
 
 for module_name in graph_modules:
